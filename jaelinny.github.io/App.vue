8a0b82d632f9b1ac27198a6e227c09813e978b09
jaelinny <leenlyn3127@gmail.com>
2024-02-15 15:24:45 +0900

    update file

---
+++ jaelinny.github.io/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-4 "
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          :text="link"
+          variant="text"
+        ></v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            flat
+            hide-details
+            label="Search"
+            rounded="lg"
+            single-line
+            variant="solo-filled"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  :key="n"
+                  link
+                  :title="`HW-knowledge Q&A`"
+		  @click="handleHWknowledgeQAClick"
+               
+		  ></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  color="grey-lighten-4"
+                  link
+                  title="WIKI"
+                ></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  const links = [
+    'Dashboard',
+    'Messages',
+    'Profile',
+    'Updates',
+  ]
+    const handleHWKnowledgeQAClick = (event) => {
+    console.log(`handleHWKnowledgeQAClick clicked!`, event);
+  };
+
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
+  }
+</script>
---
+++ jaelinny.github.io/ChangeLog.md
@@ -0,0 +1,33 @@
+# ChangeLog.md
+
+
+### 0.1.0
+- [x] study html a tag
+
+### 0.2.0
+- [x] search-consol
+
+### 0.3.0
+- [x] NAVER search-adviso
+
+### 0.4.0
+- [ ] [SEO NAVER](https://github.com/Jaelinny/jaelinny.github.io/issues/4
+
+### 0.4.1
+- [x] SEO-NAVER 1+
+
+### 0.4.2
+- [x] HTML
+
+### 0.4.3
+- [x] HTML-content
+
+### 0.4.4
+- [x] SEO
+
+### 0.5.0
+- [ ] firebase & deploy
+
+### 0.6.0
+- [x] Styduy CSS
+- [x] Resume html
